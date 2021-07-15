28e5fecf1a9ee22c64e38388afb8543a68285224
ciro.c <cirofalsarella@gmail.com>
2021-07-14 22:44:53 -0300

    json lib to help with api requests

--- src/com/dashboard/data/importer/Requests.java
+++ src/com/dashboard/data/importer/Requests.java
@@ -1,11 +1,13 @@
 package com.dashboard.data.importer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.json.JSONObject;
+import javax.xml.*;
+
 import java.util.Base64;
- 
+
 public class Requests {
 
 	public static String Vaccination () {
@@ -14,19 +16,9 @@ public class Requests {
             // Getting the API data
     		URL url = new URL("https://api.github.com/repos/owid/covid-19-data/contents/public/data/vaccinations/country_data/Brazil.csv");
             String response = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8")).readLine();
-            
-            // Selecting the desired field
-            content = response.split(",")[9].split(":")[1];
-            content = content.substring(10, content.length() - 1);
-            System.out.println(content);
-            
-            // Decoding from base64
-            Base64.Decoder decoder = Base64.getDecoder();
-            byte[] bytes = content.getBytes();
-            bytes = decoder.decode(bytes);
-            content = new String(bytes);
-            
-    	} catch (IOException ioe) {
+            JSONObject json = new JSONObject(response);
+
+    	} catch (Exception ioe) {
             System.out.println(ioe.getMessage());
     	}
     	
