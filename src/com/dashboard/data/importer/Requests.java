122a1dcd7e0d58c1f61a70b31b21a1bb2cbed3c6
PedroMartelleto <martelleto.pedro@gmail.com>
2021-07-18 20:41:06 -0300

    Added token from dummy account

--- src/com/dashboard/data/importer/Requests.java
+++ src/com/dashboard/data/importer/Requests.java
@@ -10,7 +10,7 @@ import org.kohsuke.github.GitHubBuilder;
 public class Requests {
 	public static String vaccination() {
 		try {
-			GitHub github = new GitHubBuilder().withOAuthToken("INSERT_TOKEN_HERE").build();
+			GitHub github = new GitHubBuilder().withOAuthToken("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").build();
 			GHRepository repo = github.getUser("CSSEGISandData").getRepository("COVID-19");
 			GHContent content = repo.getFileContent("csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv");
 			System.out.println(content.getContent()); // TODO: This is deprecated, use another method instead
