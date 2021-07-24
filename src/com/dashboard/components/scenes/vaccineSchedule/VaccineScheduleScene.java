61cec509cd9a5cacd1aec554b7012f5caac797bc
PedroMartelleto <martelleto.pedro@gmail.com>
2021-07-24 17:18:56 -0300

    Finished vaccine schedule page.

--- src/com/dashboard/components/scenes/vaccineSchedule/VaccineScheduleScene.java
+++ src/com/dashboard/components/scenes/vaccineSchedule/VaccineScheduleScene.java
@@ -3,39 +3,54 @@ package com.dashboard.components.scenes.vaccineSchedule;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.lang.reflect.Field;
 
 import com.dashboard.utils.FXMLUtils;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 /**
  * Covid-related news.
  */
 public class VaccineScheduleScene extends AnchorPane {
+	private static final String VACCINE_SCHEDULE_FILENAME = "Cronograma_21Julho2021.csv";
+
 	@FXML
-	private TableView<String> scheduleTable;
+	private TableView<VaccineScheduleTableRow> scheduleTable;
 
 	@FXML
 	private void initialize() {
 		try {
-			// TODO: Fix this
-			BufferedReader reader = new BufferedReader(new FileReader(new File("com/dashboard/resources/Cronograma_21Julho2021.csv")));
-			
+			BufferedReader reader = new BufferedReader(
+					new FileReader(new File("src/com/dashboard/resources/" + VACCINE_SCHEDULE_FILENAME)));
+			Field[] columnNicknames = VaccineScheduleTableRow.class.getDeclaredFields();
+
 			String line = null;
-			boolean isFirstRow = false;
+			boolean isFirstRow = true;
+
+			// For each line in the csv...
 			while ((line = reader.readLine()) != null) {
 				if (isFirstRow) {
 					String[] columnNames = line.split(",");
-					
-					for (String columnName : columnNames) {
-						scheduleTable.getColumns().add(new TableColumn<>(columnName));
+
+					// Appends columns to table view
+					for (int i = 0; i < columnNames.length; ++i) {
+						TableColumn<VaccineScheduleTableRow, String> col = new TableColumn<>(columnNames[i]);
+						col.setCellValueFactory(new PropertyValueFactory<>(columnNicknames[i].getName()));
+						scheduleTable.getColumns().add(col);
 					}
 				} else {
-	//				scheduleTable.getRowFactory().add(line.split(","));
+					String[] l = line.split(",");
+					if (l.length >= 14) {
+						scheduleTable.getItems().add(new VaccineScheduleTableRow(l[0], l[1], l[2], l[3], l[4], l[5], l[6],
+								l[7], l[8], l[9], l[10], l[11], l[12], l[13]));
+					}
 				}
+				isFirstRow = false;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
---
+++ src/com/dashboard/components/scenes/vaccineSchedule/VaccineScheduleTableRow.java
@@ -0,0 +1,154 @@
+package com.dashboard.components.scenes.vaccineSchedule;
+
+/**
+ * Represents a table row in the vaccine schedule csv. This class is (mostly)
+ * auto-generated.
+ */
+public class VaccineScheduleTableRow {
+	private String anvisaStatus;
+	private String vaccineName;
+	private String laboratory;
+	private String jan21;
+	private String fev21;
+	private String mar21;
+	private String abr21;
+	private String mai21;
+	private String jun21;
+	private String jul21;
+	private String ago21;
+	private String afterAgo21;
+	private String total;
+	private String situation;
+
+	public VaccineScheduleTableRow(String anvisaStatus, String vaccineName, String laboratory, String jan21,
+			String fev21, String mar21, String abr21, String mai21, String jun21, String jul21, String ago21,
+			String afterAgo21, String total, String situation) {
+		super();
+		this.anvisaStatus = anvisaStatus;
+		this.vaccineName = vaccineName;
+		this.laboratory = laboratory;
+		this.jan21 = jan21;
+		this.fev21 = fev21;
+		this.mar21 = mar21;
+		this.abr21 = abr21;
+		this.mai21 = mai21;
+		this.jun21 = jun21;
+		this.jul21 = jul21;
+		this.ago21 = ago21;
+		this.afterAgo21 = afterAgo21;
+		this.total = total;
+		this.situation = situation;
+	}
+
+	public String getAnvisaStatus() {
+		return anvisaStatus;
+	}
+
+	public void setAnvisaStatus(String anvisaStatus) {
+		this.anvisaStatus = anvisaStatus;
+	}
+
+	public String getVaccineName() {
+		return vaccineName;
+	}
+
+	public void setVaccineName(String vaccineName) {
+		this.vaccineName = vaccineName;
+	}
+
+	public String getLaboratory() {
+		return laboratory;
+	}
+
+	public void setLaboratory(String laboratory) {
+		this.laboratory = laboratory;
+	}
+
+	public String getJan21() {
+		return jan21;
+	}
+
+	public void setJan21(String jan21) {
+		this.jan21 = jan21;
+	}
+
+	public String getFev21() {
+		return fev21;
+	}
+
+	public void setFev21(String fev21) {
+		this.fev21 = fev21;
+	}
+
+	public String getMar21() {
+		return mar21;
+	}
+
+	public void setMar21(String mar21) {
+		this.mar21 = mar21;
+	}
+
+	public String getAbr21() {
+		return abr21;
+	}
+
+	public void setAbr21(String abr21) {
+		this.abr21 = abr21;
+	}
+
+	public String getMai21() {
+		return mai21;
+	}
+
+	public void setMai21(String mai21) {
+		this.mai21 = mai21;
+	}
+
+	public String getJun21() {
+		return jun21;
+	}
+
+	public void setJun21(String jun21) {
+		this.jun21 = jun21;
+	}
+
+	public String getJul21() {
+		return jul21;
+	}
+
+	public void setJul21(String jul21) {
+		this.jul21 = jul21;
+	}
+
+	public String getAgo21() {
+		return ago21;
+	}
+
+	public void setAgo21(String ago21) {
+		this.ago21 = ago21;
+	}
+
+	public String getAfterAgo21() {
+		return afterAgo21;
+	}
+
+	public void setAfterAgo21(String afterAgo21) {
+		this.afterAgo21 = afterAgo21;
+	}
+
+	public String getTotal() {
+		return total;
+	}
+
+	public void setTotal(String total) {
+		this.total = total;
+	}
+
+	public String getSituation() {
+		return situation;
+	}
+
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+}
