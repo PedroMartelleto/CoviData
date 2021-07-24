c8a908abffd956a59ad0473746e2267997a7c88d
PedroMartelleto <martelleto.pedro@gmail.com>
2021-07-24 15:43:12 -0300

    Selected scenes visual display in menu

--- src/com/dashboard/components/menuBar/DashboardMenuBar.java
+++ src/com/dashboard/components/menuBar/DashboardMenuBar.java
@@ -1,15 +1,11 @@
 package com.dashboard.components.menuBar;
 
-import com.dashboard.components.scenes.infected.InfectedScene;
-import com.dashboard.components.scenes.newsReports.NewsReportsScene;
-import com.dashboard.components.scenes.vaccinations.VaccinationsScene;
-import com.dashboard.components.scenes.vaccineSchedule.VaccineScheduleScene;
 import com.dashboard.main.Scenes;
-import com.dashboard.utils.CSSUtils;
 import com.dashboard.utils.FXMLUtils;
 
+import javafx.beans.NamedArg;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -17,64 +13,91 @@ import javafx.stage.Stage;
 
 /**
  * Menu bar with buttons for navigation between scenes.
+ * 
  * @author Pedro
  */
 public class DashboardMenuBar extends HBox {
+	private static final PseudoClass SELECTED_PSEUDO_CLASS = PseudoClass.getPseudoClass("selected");
 
-    @FXML
-    private Button vaccinationsButton;
-    
-    @FXML
-    private Button infectedButton;
+	@FXML
+	private Button vaccinationsButton;
 
-    @FXML
-    private Button newsButton;
-    
-    @FXML
-    private Button vaccineScheduleButton;
-    
-    /**
-     * Called during initialization.
-     */
-    @FXML
-    private void initialize() {
-    	vaccinationsButton.setOnMouseClicked(event -> {
-    		setWindowScene(Scenes.getVaccinations());
-    	});
-    	
-    	infectedButton.setOnMouseClicked(event -> {
-    		setWindowScene(Scenes.getInfected());
-    	});
-    	
-    	newsButton.setOnMouseClicked(event -> {
-    		setWindowScene(Scenes.getNewsReport());
-    	});
-    	
-    	vaccineScheduleButton.setOnMouseClicked(event -> {
-    		setWindowScene(Scenes.getVaccineSchedule());
-    	});
-    }
-	
-    public DashboardMenuBar() {
-    	FXMLUtils.loadFXML(this);
-    }
-    
-    /**
-     * Sets the currently displayed scene.
-     * @param parent
-     */
-    private void setWindowScene(Scene scene) {
-    	Stage window = (Stage) getScene().getWindow();
-    	
-    	// Checks if we actually need to change scenes
-    	String currentClassName = window.getScene().getRoot().getClass().getName();
-    	String newClassName = scene.getRoot().getClass().getName();
+	@FXML
+	private Button infectedButton;
 
-    	if (currentClassName.equals(newClassName)) {
-    		// Nothing to do!
-    		return;
-    	}
-    	
+	@FXML
+	private Button newsButton;
+
+	@FXML
+	private Button vaccineScheduleButton;
+
+	private String viewSelected;
+
+	public DashboardMenuBar(@NamedArg("selected") String selected) {
+		super();
+		
+		FXMLUtils.loadFXML(this);
+		viewSelected = selected;
+		
+		if (viewSelected != null) {
+			switch (viewSelected) {
+			case "vaccinations":
+				vaccinationsButton.pseudoClassStateChanged(SELECTED_PSEUDO_CLASS, true);
+				break;
+			case "infected":
+				infectedButton.pseudoClassStateChanged(SELECTED_PSEUDO_CLASS, true);
+				break;
+			case "news":
+				newsButton.pseudoClassStateChanged(SELECTED_PSEUDO_CLASS, true);
+				break;
+			case "vaccineSchedule":
+				vaccineScheduleButton.pseudoClassStateChanged(SELECTED_PSEUDO_CLASS, true);
+				break;
+			}
+		} else {
+			vaccinationsButton.pseudoClassStateChanged(SELECTED_PSEUDO_CLASS, true);
+		}
+	}
+
+	/**
+	 * Called during initialization.
+	 */
+	@FXML
+	private void initialize() {
+		vaccinationsButton.setOnMouseClicked(event -> {
+			setWindowScene(Scenes.getVaccinations(), vaccinationsButton);
+		});
+
+		infectedButton.setOnMouseClicked(event -> {
+			setWindowScene(Scenes.getInfected(), infectedButton);
+		});
+
+		newsButton.setOnMouseClicked(event -> {
+			setWindowScene(Scenes.getNewsReport(), newsButton);
+		});
+
+		vaccineScheduleButton.setOnMouseClicked(event -> {
+			setWindowScene(Scenes.getVaccineSchedule(), vaccineScheduleButton);
+		});
+	}
+
+	/**
+	 * Sets the currently displayed scene.
+	 * 
+	 * @param parent
+	 */
+	private void setWindowScene(Scene scene, Button button) {
+		Stage window = (Stage) getScene().getWindow();
+
+		// Checks if we actually need to change scenes
+		String currentClassName = window.getScene().getRoot().getClass().getName();
+		String newClassName = scene.getRoot().getClass().getName();
+
+		if (currentClassName.equals(newClassName)) {
+			// Nothing to do!
+			return;
+		}
+
 		window.setScene(scene);
-    }
+	}
 }
